f38e2d084d246d1309ac52f5cce1cbd1e0e5b62d
Soumyajit Chakraborty <soumyajit.chakraborty@Vanderbilt.Edu>
2023-09-20 23:55:29 -0500

    Initial Commit

---
+++ Assets/Scripts/Disconnect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Disconnect : MonoBehaviour
+{
+    //Leave the room.
+    public void DisconectButton()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class GamePlayController : MonoBehaviour
+{
+    private PhotonView PV;
+    public TMP_Text txtNumber;
+    int globalNumber;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        txtNumber.text = "Number to Increase: " + globalNumber;
+    }
+
+    public void IncreaseNumber()
+    {
+        PV.RPC("RPC_IncreaseNumber", RpcTarget.AllViaServer);
+    }
+
+    [PunRPC]
+    void RPC_IncreaseNumber()
+    {
+        globalNumber++;
+    }
+}
